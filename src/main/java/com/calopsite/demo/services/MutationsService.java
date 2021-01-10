86dce1aab11d0bbaea4c0749b5623a936b626d2c
igor ferreira <igoreira0@gmail.com>
2021-01-09 22:39:00 -0300

    fixed variable with name error

--- src/main/java/com/calopsite/demo/services/MutationsService.java
+++ src/main/java/com/calopsite/demo/services/MutationsService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class NameMutations {
+public class MutationsService {
     @Autowired
     private MutationRepository mutRepository;
 
